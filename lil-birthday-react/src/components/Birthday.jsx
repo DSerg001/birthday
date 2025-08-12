2938288a3beaa20564ae5086a51342bbfbd11fa6
DSerg001 <danielyanserg2009@gmail.com>
2025-08-12 15:08:49 +0300

    new update!

--- lil-birthday-react/src/components/Birthday.jsx
+++ lil-birthday-react/src/components/Birthday.jsx
@@ -52,6 +52,7 @@ function BirthdayCard() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
+            className="iframe"
           ></iframe>
         </section>
       </main>
